9f22228df0b6c68f90f1909049ad2175b3c7460f
mikitob <mislav.bilic12@gmail.com>
2024-12-18 21:38:28 +0100

    Update 3_modify_tables.sql for table modifications

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -13,17 +13,19 @@ NOTE: If you are having issues with permissions. And you get error:
     1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
-\copy company_dim FROM '/Users/mislavbilic/Downloads/Data Analysis Projects/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy company_dim FROM '/Users/mislavbilic/Documents/VS Code/Data Projects/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_dim FROM '/Users/mislavbilic/Downloads/Data Analysis Projects/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_dim FROM '/Users/mislavbilic/Documents/VS Code/Data Projects/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy job_postings_fact FROM '/Users/mislavbilic/Downloads/Data Analysis Projects/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy job_postings_fact FROM '/Users/mislavbilic/Documents/VS Code/Data Projects/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_job_dim FROM '/Users/mislavbilic/Downloads/Data Analysis Projects/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_job_dim FROM '/Users/mislavbilic/Documents/VS Code/Data Projects/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
-
 -- NOTE: This has been updated from the video to fix issues with encoding
+-- SELECT * FROM company_dim;
+
+-- SELECT * FROM job_postings_fact LIMIT 100;
 
 COPY company_dim
 FROM '/Users/mislavbilic/Downloads/Data Analysis Projects/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv'
